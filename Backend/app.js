d24146f5a3bf14df64c7279548afcc5bcff1b080
Johann <Joh.rackelboom@gmail.com>
2024-05-17 15:28:26 +0200

    add call API

--- Backend/app.js
+++ Backend/app.js
@@ -1,33 +1,60 @@
 const express = require("express");
+const mongoose = require('mongoose');
+
+const Book = require("./models/Book");
 
 const app = express();
+
+mongoose.connect('mongodb+srv://johrackelboom:vCpouKW51nKvjk5x@cluster-first.wsmhmrt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-first',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-    next();
-  });
-
-app.post("/api/books", (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: "Objet crée !"
-    })
-})
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  next();
+});
 
 app.get("/api/books", (req, res, next) => {
-    const book = [
-      {
-        _id: "oeihfzeoi",
-        title: "blabla le retour",
-        author: "JR Rack",
-        imageUrl: "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      },
-    ];
-    res.status(200).json(book);
+  Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({ error }));
+});
+
+
+app.get('/api/books/:id', (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.post("/api/books", (req, res, next) => {
+  delete req.body._id;
+  const book = new Book({
+    ...req.body
+  });
+  book.save()
+    .then(() => res.status(201).json({ message: 'Livre enregistré !' }))
+    .catch(error => res.status(400).json({ error }));
+})
+
+app.put('/api/books/:id', (req, res, next) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Livre modifié !' }))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/books/:id', (req, res, next) => {
+  Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Livre supprimé !' }))
+    .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
