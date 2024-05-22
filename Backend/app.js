a34c4bdb5502b3e7138e45f212176f5cca8197e7
Johann <Joh.rackelboom@gmail.com>
2024-05-22 13:13:40 +0200

    add router/controller

--- Backend/app.js
+++ Backend/app.js
@@ -1,19 +1,21 @@
-const express = require("express");
-const mongoose = require('mongoose');
+require("dotenv").config();
 
-const Book = require("./models/Book");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const MONGO_URI = process.env.MONGO_URI;
+
+const booksRoutes = require("./routes/books");
+
+
+mongoose.connect(MONGO_URI,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true,})
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
-
-mongoose.connect('mongodb+srv://johrackelboom:vCpouKW51nKvjk5x@cluster-first.wsmhmrt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-first',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -22,39 +24,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/api/books", (req, res, next) => {
-  Book.find()
-    .then(books => res.status(200).json(books))
-    .catch(error => res.status(400).json({ error }));
-});
+app.use(bodyParser.json());
 
-
-app.get('/api/books/:id', (req, res, next) => {
-  Book.findOne({ _id: req.params.id })
-    .then(book => res.status(200).json(book))
-    .catch(error => res.status(404).json({ error }));
-});
-
-app.post("/api/books", (req, res, next) => {
-  delete req.body._id;
-  const book = new Book({
-    ...req.body
-  });
-  book.save()
-    .then(() => res.status(201).json({ message: 'Livre enregistré !' }))
-    .catch(error => res.status(400).json({ error }));
-})
-
-app.put('/api/books/:id', (req, res, next) => {
-  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Livre modifié !' }))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.delete('/api/books/:id', (req, res, next) => {
-  Book.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Livre supprimé !' }))
-    .catch(error => res.status(400).json({ error }));
-});
+app.use("/api/books", booksRoutes);
 
 module.exports = app;
