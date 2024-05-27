dede0c87a37a61c1e4505ccb74325feedd5efc4a
Johann <Joh.rackelboom@gmail.com>
2024-05-27 10:46:56 +0200

    add sharp for compress

---
+++ Backend/middleware/sharp-config.js
@@ -0,0 +1,26 @@
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = async (req, res, next) => {
+    if (!req.file) {
+        return res.status(400).send('Pas de fichier reçu');
+    }
+
+    try {
+        const filePath = path.join(__dirname, 'images', req.file.filename);
+        const outputFilePath = path.join(__dirname, 'images', `${req.file.filename.split('.')[0]}.webp`);
+
+        await sharp(filePath)
+            .webp({ quality: 80 })
+            .toFile(outputFilePath);
+
+
+        fs.unlinkSync(filePath);
+
+        req.file.filename = `${req.file.filename.split('.')[0]}.webp`;
+        next();
+    } catch (error) {
+        res.status(500).send({ error });
+    }
+};
--- Backend/routes/books.js
+++ Backend/routes/books.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const auth = require("../middleware/auth")
 const multer = require("../middleware/multer-config")
+const sharp = require("../middleware/sharp-config")
 const router = express.Router();
 
 const booksCtrl = require("../controllers/books");
 
-router.post("/", auth, multer,  booksCtrl.createBook);
-router.put("/:id", auth, multer,  booksCtrl.modifyBook);
+router.post("/", auth, multer, sharp,  booksCtrl.createBook);
+router.put("/:id", auth, multer, sharp,  booksCtrl.modifyBook);
 router.delete("/:id",auth, booksCtrl.deleteBook);
 router.get("/:id", booksCtrl.getOneBook);
 router.get("/", booksCtrl.getAllBook);
